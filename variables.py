ace7b9c34cc8a5315379067ab752b190f32fb2d0
D. Sukhdeo <144715223+DeveshSukhdeo@users.noreply.github.com>
2024-09-09 09:29:55 -0400

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,4 +1,17 @@
 # Hashtags are symbols to create comments
+
 fname = "Devesh" #string variable 
 age = 55 #number variable 
-fact = true #Boolean variabe 
+fact = True #Boolean variabe 
+
+print(13) #This prints a number 
+print("We are the best") #This prints a string 
+
+#Print data from a variable
+
+print(fname)
+print(age)
+print(fact)
+
+question = input("What day is it?") 
+print(fname, "Happy " + question + "!")
