658bffc46136778b242e3d22e70e4c017a9aa1a1
D. Sukhdeo <144715223+DeveshSukhdeo@users.noreply.github.com>
2024-09-16 09:22:13 -0400

    Create CFU2.py

---
+++ folder2/CFU2.py
@@ -0,0 +1,5 @@
+fname = input("What is your first name? ")
+lname = input("What is your last name? ")
+age = input("How old are you? (Number Only) ") 
+school = input("How many years have you been in high school? ")
+print (fname, lname + " is " + age + " years old and has been in high school for " + school + " years.") 
