b524f223fa49262aa302f5580b8f632525d53169
D. Sukhdeo <144715223+DeveshSukhdeo@users.noreply.github.com>
2024-09-24 08:27:40 -0400

    Create CFU3.py

---
+++ Unit1/CFU3.py
@@ -0,0 +1,19 @@
+fname = input("Enter your first name: ")
+lname = input("Enter your last name: ") 
+
+num1 = int(input("Give me a number: "))
+num2 = int(input("Give me a number: "))
+
+print("Hello " + fname, lname + "!")  
+
+addition = num1 + num2
+subtraction = num1 - num2 
+multiplication = num1 * num2 
+division = num1 / num2 
+modulus = num1 % num2
+
+print("When you add the two numbers the result is: " + str(addition)) 
+print("When you subtract the two numbers the result is: " + str(subtraction))
+print("When you multiply the two numbers the result is: " + str(multiplication))
+print("When you divide the two numbers the result is: " + str(division))
+print("The modulus of the two numbers is: " + str(modulus)) 
