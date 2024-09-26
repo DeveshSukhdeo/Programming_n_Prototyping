0c6fb9c3496b2867020152b2705e4ca0fa93493a
D. Sukhdeo <144715223+DeveshSukhdeo@users.noreply.github.com>
2024-09-26 09:09:20 -0400

    Update CFU4.py

--- Unit1/CFU4.py
+++ Unit1/CFU4.py
@@ -9,8 +9,11 @@ print("Hello " + name + "!")
 a = float(input("Enter the a coefficient: "))
 b = float(input("Enter the b coefficient: "))
 c = float(input("Enter the c coefficient: ")) 
+
 print("Your quadratic equation is: " + str(a) + "x^2 + " + str(b) + "x + " + str(c)) 
+
 x1 = (-b + math.sqrt(b**2 - (4*a*c))) / (2*a) 
 x2 = (-b - math.sqrt(b**2 - (4*a*c))) / (2*a)
+
 print("The roots of the equation are: x1 = " + str(x1) + " x2 = " + str(x2)) 
 
