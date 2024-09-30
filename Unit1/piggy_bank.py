8eabb865bba302582b4218ea134763105934f42c
D. Sukhdeo <144715223+DeveshSukhdeo@users.noreply.github.com>
2024-09-30 08:37:06 -0400

    Update piggy_bank.py

--- Unit1/piggy_bank.py
+++ Unit1/piggy_bank.py
@@ -14,3 +14,20 @@
 # calculate the total and give the answer in decimals and in a combination 
 # i.e: $13.67 or $13 and 2 quarters, 1 dime, 1 nickle, 2 cent. 
 
+pennies = int(input("How many pennies do you have? "))
+nickles = int(input("How many nickels do you have? "))
+dimes = int(input("How many dimes do you have? "))
+quarters = int(input("How many quarters do you have? "))
+loonies = int(input("How many loonies do you have? "))
+toonies = int(input("How many toonies do you have? "))
+
+p = pennies * 0.01
+n = nickles * 0.05 
+d = dimes * 0.10
+q = quarters * 0.25
+l = loonies * 1
+t = toonies * 2 
+totaldeci = p + n + d + q + l + t
+
+print ("Your coins: " + str(toonies) + " toonies, " + str(loonies) + " loonies, " + str(quarters) + " quarters, " + str(dimes) + " dimes, " + str(nickles) + " nickles, " + str(pennies) + " pennies.")
+print ("This adds up to: $" + str(totaldeci)) 
