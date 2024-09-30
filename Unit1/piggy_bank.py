650bd876e5392b79294601361eb8d72bed8faffe
D. Sukhdeo <144715223+DeveshSukhdeo@users.noreply.github.com>
2024-09-30 08:19:03 -0400

    Create piggy_bank.py

---
+++ Unit1/piggy_bank.py
@@ -0,0 +1,16 @@
+# 9/30/24
+# Period 1-2
+# Devesh Sukhdeo
+
+# little Canadian kid broke their piggy bank
+# you are to create a program to help them figure out how much money they have 
+# pennies = 1 cent
+# nickel = 5 cents 
+# dimes = 10 cents 
+# quarters = 25 cents 
+# loonies = $1 or 100 cents 
+# toonies = $2 or 200 cents
+# ask the kid how many coins they have of each kind 
+# calculate the total and give the answer in decimals and in a combination 
+# i.e: $13.67 or $13 and 2 quarters, 1 dime, 1 nickle, 2 cent. 
+
