efc268575fcaea643e7d290b024389bd5e19ac5b
D. Sukhdeo <144715223+DeveshSukhdeo@users.noreply.github.com>
2024-10-10 09:33:24 -0400

    Update practice_code.py

--- Unit3/practice_code.py
+++ Unit3/practice_code.py
@@ -1,3 +1,11 @@
+#Conditionals
 num = int(input("Enter a number: "))
 if num > 5:
     print("The number is greater than 5!")
+
+#If - Else 
+num1 = int(input("Enter a number: " ))
+if num1 >= 5:
+    print ("This number is greater than 5.")
+else:
+    print ("This number is less than 5.")
