d7a724d9b035c597c65d58cf429bc2ff5cc42447
D. Sukhdeo <144715223+DeveshSukhdeo@users.noreply.github.com>
2024-10-10 08:21:07 -0400

    Update CFU7.py

--- Unit3/CFU7.py
+++ Unit3/CFU7.py
@@ -1,1 +1,19 @@
+#10/10/24
+#Devesh Sukhdeo
+#Period 1-2
+#Check-in for Functions 
 
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a number: "))
+num3 = int(input("Enter a number: "))
+           
+def add(num1, num2, num3):
+    print ("The sum of the numbers is: " + str(num1 + num2 + num3))
+    
+add(num1, num2, num3)
+
+def avg(num1, num2, num3):
+    print ("The average of the numbers is: " + str((num1 + num2 + num3) / 3)) 
+    
+avg(num1, num2, num3) 
+           
