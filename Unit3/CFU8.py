28acb3749938beb4f3116b545487dd077100ffab
D. Sukhdeo <144715223+DeveshSukhdeo@users.noreply.github.com>
2024-10-15 08:46:24 -0400

    Update CFU8.py

--- Unit3/CFU8.py
+++ Unit3/CFU8.py
@@ -7,8 +7,12 @@ Version 1: If the user responds “yes” - say great!
 If the user responds with anything else say: “NO?! So who is cooking?”
 '''
 
-x = str(input("Did you order delivery?"))
-if x == "yes" or x == "Yes" or x == "YES" or x == "Y" or x =="y":
+order = str(input("Did you order delivery?"))
+if order == "yes" or order == "Yes" or order == "YES" or order == "Y" or order =="y":
     print("Great!")
+    cost = float(input("What was the cost of your food? "))
+    people = int(input("How many people are splitting the order? "))
+    cpp = cost/people
+    print ("The cost per person is $" + str(cpp))
 else:
     print("NO?! So who is cooking?")
