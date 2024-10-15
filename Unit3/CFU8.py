b2014465bafd6a3a358ad98d364805044de73bf9
D. Sukhdeo <144715223+DeveshSukhdeo@users.noreply.github.com>
2024-10-15 08:26:24 -0400

    Update and rename CFY8.py to CFU8.py

--- Unit3/CFU8.py
+++ Unit3/CFU8.py
@@ -1,7 +1,11 @@
 #Devesh Sukhdeo
 #Period 1-2 
 #10/15/24
-#GrubHub Program
+'''
+GrubHub Program
+Version 1: If the user responds “yes” - say great!
+If the user responds with anything else say: “NO?! So who is cooking?”
+'''
 
 x = str(input("Did you order delivery?"))
 if x == "yes" or x == "Yes" or x == "YES" or x == "Y" or x =="y":
