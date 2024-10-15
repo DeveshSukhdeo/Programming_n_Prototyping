3dc74ca1648d6d3876c6ed01285128f405fa4421
D. Sukhdeo <144715223+DeveshSukhdeo@users.noreply.github.com>
2024-10-15 08:23:17 -0400

    Create CFY8.py

---
+++ Unit3/CFY8.py
@@ -0,0 +1,10 @@
+#Devesh Sukhdeo
+#Period 1-2 
+#10/15/24
+#GrubHub Program
+
+x = str(input("Did you order delivery?"))
+if x == "yes" or x == "Yes" or x == "YES" or x == "Y" or x =="y":
+    print("Great!")
+else:
+    print("NO?! So who is cooking?")
