57836b7239b2a751bd588c5cf9d58ead915bd42f
D. Sukhdeo <144715223+DeveshSukhdeo@users.noreply.github.com>
2024-10-17 09:04:59 -0400

    Update CFU9.py

--- Unit3/CFU9.py
+++ Unit3/CFU9.py
@@ -1,1 +1,28 @@
+#10/17/24
+#Devesh Sukhdeo
+#Period 1-2
+#Guessing game roll of the dice 
+#CFU9
+
+import random
+rolls = input("How many rolls do you want to play?")
+g_num = input("Guess a number between 1 and 6: ")
+r_num = random.randint(1,6)
+
+print("Guessed number: " + str(g_num))
+print("Random number: " + str(r_num))
+
+if g_num == r_num:
+    points = 0 
+    points2 = points + 6
+    print("Your total points are: " + str(points2))
+elif g_num != r_num: 
+    points = 0
+    points2 = points - 1 
+    print("Your total points are: " + str(points2))
+else: 
+    print("Your total points are: " + str(points))
+ 
+    
+
 
