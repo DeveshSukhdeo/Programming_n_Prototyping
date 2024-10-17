5aa12641ccd91068f616610b486b0d98c1cd5fbb
D. Sukhdeo <144715223+DeveshSukhdeo@users.noreply.github.com>
2024-10-17 09:27:43 -0400

    Update CFU9.py

--- Unit3/CFU9.py
+++ Unit3/CFU9.py
@@ -8,20 +8,17 @@ import random
 rolls = input("How many rolls do you want to play?")
 g_num = int(input("Guess a number between 1 and 6: "))
 r_num = random.randint(1,6)
+points = 0
 
 print("Guessed number: " + str(g_num))
 print("Random number: " + str(r_num))
 
 if g_num == r_num:
-    points = 0 
-    print("Your total points are: " + str(points+6))
+    points = points + 6
+    print("Your total points are: " + str(points))
 elif g_num != r_num: 
-    points = 0
-    print("Your total points are: " + str(points-1))
+    points = points - 1
+    print("Your total points are: " + str(points))
 else: 
     print("Your total points are: " + str(points))
  
-
-
-
-
