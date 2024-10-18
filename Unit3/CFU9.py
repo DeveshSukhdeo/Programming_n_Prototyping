baadf5edc21fcfd20ca910508819b9b8d2617dab
D. Sukhdeo <144715223+DeveshSukhdeo@users.noreply.github.com>
2024-10-18 09:30:51 -0400

    Update CFU9.py

--- Unit3/CFU9.py
+++ Unit3/CFU9.py
@@ -5,17 +5,26 @@
 #CFU9
 
 import random
-rolls = input("How many rolls do you want to play?")
-g_num = int(input("Guess a number between 1 and 6: "))
-r_num = random.randint(1,6)
-points = 0
+print("Let's roll the dice!")
 
-print("Guessed number: " + str(g_num))
-print("Random number: " + str(r_num))
+num_rolls = int(input("How many times do you want to roll the dice? "))
 
-if g_num == r_num:
-    points = points + 6
-    print("Your total points are: " + str(points))
-else: 
-    points = points - 1
-    print("Your total points are: " + str(points))
+def guess(num_random, total):
+    user_guess = int(input("Guess the roll: "))
+    if user_guess == num_random:
+        total = total + 6
+    else:
+        total = total - 1 
+
+def rolls(num_rolls, total = 0):
+    num_random = random.randint(1,6)
+    if num_rolls == 0:
+        return total
+    else: 
+        num_rolls = num_rolls - 1        
+        guess(num_random, total)
+        print (f"You rolled a {num_random}!")
+        return rolls(num_rolls, total)
+    
+total = rolls(num_rolls)
+print(f"The total is: {total}") 
