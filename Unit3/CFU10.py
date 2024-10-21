91f334e7780d4b3aace172be43be2e34a2ef890e
D. Sukhdeo <144715223+DeveshSukhdeo@users.noreply.github.com>
2024-10-21 08:41:31 -0400

    Update CFU10.py

--- Unit3/CFU10.py
+++ Unit3/CFU10.py
@@ -11,7 +11,7 @@ def guess(num_random, num_attempts):
     guess_num = int(input("Guess the random number (1-10)")) 
     num_attempts = num_attempts + 1
     if guess_num == num_random: 
-        print("Your guess is correct, you made " + num_attempts + " attempts.")
+        print("Your guess is correct, you made " + str(num_attempts) + " attempts.")
     elif guess_num > num_random:
         print("Your guess is too high.")
         return guess
@@ -21,4 +21,4 @@ def guess(num_random, num_attempts):
     else: 
         return guess
     
- 
+guess(num_random, num_attempts)
