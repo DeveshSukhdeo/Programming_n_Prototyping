a59a0944a073386211b0e54d3c6cd28977c37cf2
D. Sukhdeo <144715223+DeveshSukhdeo@users.noreply.github.com>
2024-10-25 09:34:09 -0400

    Update CFU11.py

--- Unit4/CFU11.py
+++ Unit4/CFU11.py
@@ -1,1 +1,24 @@
+#10/25/24
+#Devesh Sukhdeo
+#Period 1-2
+#CFU #11
 
+
+
+
+#for i in range(10, 71, 10) :
+#    print(i)
+    
+
+#for x in range(0, 10, -0.5):
+#    print (x)
+
+
+# need to create a function
+def beer ():
+  
+for b in range(99, 0, -1):
+    print (f"{b} bottles of beer on the wall, {b} bottles of beer")
+    if 
+    print ("Take one down, pass it round 98 bottles of beer")
+   
