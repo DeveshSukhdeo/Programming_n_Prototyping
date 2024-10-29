c564d980d8302d2434c6a2d4877ab0851bedaa78
D. Sukhdeo <144715223+DeveshSukhdeo@users.noreply.github.com>
2024-10-29 07:26:49 -0400

    Update CFU11.py

--- Unit4/CFU11.py
+++ Unit4/CFU11.py
@@ -3,22 +3,36 @@
 #Period 1-2
 #CFU #11
 
+run = int(input("How many loops do you want to run? 1-3 "))
+
+def count1(c):
+    for c in range(10, 71, 10):
+        print(c)
+    print(" ")
 
 
+def half_count(x): 
+    for x in range(0, 21):
+         print(x * 0.5)
+    print(" ")
 
-#for i in range(10, 71, 10) :
-#    print(i)
-    
+def beer(i):            
+    for i in range(99, 0, -1):
+        if i > 1:
+            print(f"{i} bottles of beer on the wall, {i} bottles of beer.")
+            print(f"Take one down and pass it around, {i - 1} bottles of beer on the wall.\n")
+        else:
+            print(f"{i} bottle of beer on the wall, {i} bottle of beer.")
+            print("Take one down and pass it around, no more bottles of beer on the wall.\n")            
+    print("No more bottles of beer on the wall, no more bottles of beer.")
+    print("Go to the store and buy some more, 99 bottles of beer on the wall.")
 
-#for x in range(0, 10, -0.5):
-#    print (x)
-
-
-# need to create a function
-def beer ():
-  
-for b in range(99, 0, -1):
-    print (f"{b} bottles of beer on the wall, {b} bottles of beer")
-    if 
-    print ("Take one down, pass it round 98 bottles of beer")
-   
+if run == 1:
+    count1(1)
+elif run == 2:
+    count1(1)
+    half_count(1)
+else:
+    count1(1)
+    half_count(1)
+    beer(1) 
