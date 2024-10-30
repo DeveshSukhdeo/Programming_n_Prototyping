5c2983a0a7efef80b16a2b95ca55cb7a0920616d
D. Sukhdeo <144715223+DeveshSukhdeo@users.noreply.github.com>
2024-10-30 08:14:25 -0400

    Update CFU12.py

--- Unit4/CFU12.py
+++ Unit4/CFU12.py
@@ -14,17 +14,32 @@ Modify Version 1 so that the User gets only 3 chances.
 '''
 #CFU #12
 
-password = "simonsnyc"
-pword = input("What is the password")
-count = 0
+version = int(input("What version do you want to run? "))
 
-while pword != password and count < 2:
-    print ("Wrong Password!")
+def ver1(version):
+    password = "simonsnyc"
     pword = input("What is the password")
-    count = count + 1
-if pword == password:
+    while pword != password:
+        print ("Wrong Password!")
+        pword = input("What is the password")
     print ("Correct, you may enter...")
-else: 
-    print ("Wrong Password!")
 
-    
+
+def ver2(version):
+    password = "simonsnyc"
+    pword = input("What is the password")
+    count = 0
+
+    while pword != password and count < 2:
+        print ("Wrong Password!")
+        pword = input("What is the password")
+        count = count + 1
+    if pword == password:
+        print ("Correct, you may enter...")
+    else: 
+        print ("Wrong Password!")
+
+if version == 1:
+    ver1(1)
+else:
+    ver2(1)
