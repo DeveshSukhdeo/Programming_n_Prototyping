7ad8ef4988294cbb3d81154137e85f3b6c6fccbe
D. Sukhdeo <144715223+DeveshSukhdeo@users.noreply.github.com>
2024-12-02 09:11:55 -0500

    Update thanksgiving_drawing.py

--- Unit6/thanksgiving_drawing.py
+++ Unit6/thanksgiving_drawing.py
@@ -5,15 +5,29 @@
 import simplegui 
 
 def draw(canvas):
-    canvas.draw_circle((300, 280), 90, 5, "yellow", "brown")
-    canvas.draw_circle((300, 180), 55, 5, "yellow", "brown")
-    canvas.draw_circle((280, 165), 10, 1, "yellow", "white")
-    canvas.draw_circle((320, 165), 10, 1, "yellow", "white")
+    #Polygons (Feathers)
+    
+    
+    #Cirlces (Make the body)
+    canvas.draw_circle((300, 280), 85, 5, "brown", "brown")
+    canvas.draw_circle((300, 170), 55, 5, "brown", "brown")
+    #Circles (Make eyes)
+    canvas.draw_circle((280, 155), 10, 1, "white", "white")
+    canvas.draw_circle((320, 155), 10, 1, "white", "white")
+    
+    #Circle & Points (Make the pupil of the eyes)
+    canvas.draw_circle((281, 156), 5, 1, "black", "black")
+    canvas.draw_circle((319, 156), 5, 1, "black", "black")
+    canvas.draw_point([282, 154], "white")
+    canvas.draw_point([320, 154], "white")
+    
+    #Polygon (Beak)
+    canvas.draw_polygon([(290, 170), (300, 180), (310, 170)], 1, "yellow", "yellow")
    
     
  
 #Create the frame 
 frame = simplegui.create_frame("Thanksgiving Drawing", 600, 400) 
-frame.set_canvas_background("white")
-frame.set_draw_handler(draw) 
+frame.set_canvas_background("skyblue")
+frame.set_draw_handler(draw)
 frame.start()
