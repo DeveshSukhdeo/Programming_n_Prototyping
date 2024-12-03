213f0123ec0bb41951070516a44ac3caa02af717
D. Sukhdeo <144715223+DeveshSukhdeo@users.noreply.github.com>
2024-12-02 21:26:39 -0500

    Update thanksgiving_drawing.py

--- Unit6/thanksgiving_drawing.py
+++ Unit6/thanksgiving_drawing.py
@@ -10,48 +10,56 @@ import simplegui
 #Frame Setup 
 frame = simplegui.create_frame("Thanksgiving Drawing", 600, 400)
 #Set Frame Background 
-frame.set_canvas_background("skyblue")
+frame.set_canvas_background("#9fd8ef")
 
 #Create function for the feathers of the turkey
 def feathers(canvas):
     #Polygons (Feathers)
-    canvas.draw_polygon([(280, 220), (100, 230), (270, 340)], 2, "black", "blue")
-    canvas.draw_polygon([(290, 150), (130, 80), (280, 280)], 2, "black", "yellow")
-    canvas.draw_polygon([(250, 150), (300, 20), (350, 150)], 2, "black", "pink")
-    canvas.draw_polygon([(310, 150), (470, 80), (320, 280)], 2, "black", "red")
-    canvas.draw_polygon([(320, 220), (500, 230), (330, 340)], 2, "black", "green")
+    canvas.draw_polygon([(280, 220), (100, 230), (270, 340)], 2, "black", "#c1311c")
+    canvas.draw_polygon([(290, 150), (130, 80), (280, 280)], 2, "black", "#ebba28")
+    canvas.draw_polygon([(250, 150), (300, 20), (350, 150)], 2, "black", "#c1311c")
+    canvas.draw_polygon([(310, 150), (470, 80), (320, 280)], 2, "black", "#ebba28")
+    canvas.draw_polygon([(320, 220), (500, 230), (330, 340)], 2, "black", "#c1311c")
 
 #Create function for the body of the turkey     
 def turkey_body(canvas):
     #Cirlces (Make the body)
-    canvas.draw_circle((300, 280), 85, 5, "brown", "brown")
-    canvas.draw_circle((300, 170), 55, 5, "brown", "brown")
+    canvas.draw_circle((300, 280), 85, 1, "black", "#a26b35")
+    canvas.draw_circle((300, 170), 55, 1, "black", "#a26b35")
 
 #Create function for the eyes of the turkey     
-def turkey_eyes(canvas):
+def turkey_face(canvas):
     #Circles (Make eyes)
-    canvas.draw_circle((280, 155), 10, 1, "white", "white")
-    canvas.draw_circle((320, 155), 10, 1, "white", "white")
+    canvas.draw_circle((280, 155), 10, 1, "black", "white")
+    canvas.draw_circle((320, 155), 10, 1, "black", "white")
     #Circle (Make the pupil of the eyes)
     canvas.draw_circle((281, 156), 5, 1, "black", "black")
     canvas.draw_circle((319, 156), 5, 1, "black", "black")
     #Points (Give eyes shine) 
     canvas.draw_point([282, 154], "white")
     canvas.draw_point([320, 154], "white")
-    
-#Create function for extra details
-def add_ons(canvas):
     #Polygon (Beak)
-    canvas.draw_polygon([(290, 170), (300, 180), (310, 170)], 1, "yellow", "yellow")
-    #Lines (Feet)
-    canvas.draw_line([50, 50], [150, 150], 5, "purple")
+    canvas.draw_polygon([(290, 170), (300, 180), (310, 170)], 1, "#ffc002", "#ffc002")
+    
+def turkey_feet(canvas):
+    #Lines (Left Feet)
+    canvas.draw_line([270, 350], [264, 375], 7, "#ffc002")
+    canvas.draw_line([270, 350], [252, 370], 7, "#ffc002")
+    canvas.draw_line([270, 350], [277, 375], 7, "#ffc002")
+    #Lines (Right Feet)
+    canvas.draw_line([330, 350], [336, 375], 7, "#ffc002")
+    canvas.draw_line([330, 350], [348, 370], 7, "#ffc002")
+    canvas.draw_line([330, 350], [323, 375], 7, "#ffc002")
+    
+def background(canvas):
+    
 
 #Create a function to call all other functions in draw_handler    
 def draw(canvas):
+    turkey_feet(canvas)
     feathers(canvas)
     turkey_body(canvas)
-    turkey_eyes(canvas)
-    add_ons(canvas)
+    turkey_face(canvas)
     
 #Assign draw handler to frame 
 frame.set_draw_handler(draw)
