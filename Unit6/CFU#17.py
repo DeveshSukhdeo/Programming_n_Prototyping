28ce7d0cfcc08ebc5cc7ac46dc3939054e91821d
D. Sukhdeo <144715223+DeveshSukhdeo@users.noreply.github.com>
2024-12-06 09:29:53 -0500

    Create CFU#17.py

---
+++ Unit6/CFU#17.py
@@ -0,0 +1,20 @@
+# Devesh Sukhdeo 
+# Period 1-2
+# CFU 17
+# Use functions to draw text and shapes 
+
+import simplegui
+
+message = "Hello" 
+color1 = input("Enter color")
+frame_width = int(input("Initial Width?"))
+frame_height = int(input("Initial Height?"))
+
+def draw(canvas): 
+    canvas.draw_line([0,0], [100, 100], 3, color1)
+    canvas.draw_text(message, [frame_width/2, frame_height/2], 30, color1)
+    
+frame = simplegui.create_frame("CFU #17", frame_width, frame_height)
+frame.set_draw_handler(draw)
+
+frame.start()
