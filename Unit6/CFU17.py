1a8e89beb4bd59f6a2cae1590ba034ee6a2871a4
D. Sukhdeo <144715223+DeveshSukhdeo@users.noreply.github.com>
2024-12-09 09:33:10 -0500

    Update CFU17.py

--- Unit6/CFU17.py
+++ Unit6/CFU17.py
@@ -18,13 +18,21 @@ show_triangle = True
 show_ellipse = True                            
 
 # Shape drawing Functions
-
 def draw_triangle(canvas, cx, cy, size):
     half_size = size / 2
-    canvas.draw_polygon([(cx, cy - half_size),
-                         (cx - half_size, cy + half_size),
-                         (cx + half_size, cy + half_size)], 
-                          2, "Blue", "Blue") #Blue Triangle
+    canvas.draw_polygon([(cx, cy - half_size), (cx - half_size, cy + half_size), (cx + half_size, cy + half_size)], 2, "Blue", "Blue") #Blue Triangle
+    
+def draw_square(canvas, cx, cy, size):
+    half_size = size / 2
+    canvas.draw_polygon([(cx, cy - half_size), (cx - half_size, cy + half_size), (cx + half_size, cy + half_size)], 2, "Green", "Green") #Green Square
+    
+def draw_circle(canvas, cx, cy, size):
+    half_size = size / 2
+    canvas.draw_polygon([(cx, cy - half_size), (cx - half_size, cy + half_size), (cx + half_size, cy + half_size)], 2, "Red", "Red") #Red Circle
+    
+def draw_ellipse(canvas, cx, cy, size):
+    half_size = size / 2
+    canvas.draw_polygon([(cx, cy - half_size), (cx - half_size, cy + half_size), (cx + half_size, cy + half_size)], 2, "Yellow", "Yellow") #Yellow Ellipse
 
 # Draw Function
 def draw(canvas):
@@ -34,12 +42,27 @@ def draw(canvas):
                          
     #Draw each shape in a different quadrant if toggled 
     if show_triangle:
-        draw_triangle(canvas, quadrant_width / 2, frame_height - quadrant_height / 2, 100) #Triangle in bottom-left 
+        draw_triangle(canvas, quadrant_width / 2, frame_height - quadrant_height / 2, 100) #Triangle in bottom-left      
+    elif show_square: 
+        draw_square(canvas, quadrant_width / 2, frame_height - quadrant_height, 100)
+    
                          
 # Button Handlers
+def toggle_square():
+    global show_square
+    show_triangle = not show_square
+    
+def toggle_circle():
+    global show_circle
+    show_triangle = not show_circle
+    
 def toggle_triangle():
     global show_triangle
     show_triangle = not show_triangle
+       
+def toggle_ellipse():
+    global show_ellipse
+    show_triangle = not show_ellipse 
 
 # Frame Creation
 def create_frame():
@@ -47,7 +70,10 @@ def create_frame():
     frame = simplegui.create_frame("Shape Drawing Guide with Toggles", frame_width, frame_height)
     frame.set_draw_handler(draw)
                          
+    frame.add_button("Toggle Square", toggle_square, 150)
+    frame.add_button("Toggle Circle", toggle_circle, 150)
     frame.add_button("Toggle Triangle", toggle_triangle, 150)
+    frame.add_button("Toggle Ellipse", toggle_ellipse, 150)
                          
     frame.start()
                          
