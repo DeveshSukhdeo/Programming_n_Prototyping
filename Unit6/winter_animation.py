8f239c5996d93413bd090a4f131accc60cb512f4
D. Sukhdeo <144715223+DeveshSukhdeo@users.noreply.github.com>
2024-12-12 09:32:30 -0500

    Update winter_animation.py

--- Unit6/winter_animation.py
+++ Unit6/winter_animation.py
@@ -10,7 +10,7 @@ import random
 snow_y = []
 #Constants for window frame (size), snow
 width = 600
-height = 400 
+height = 600 
 snow_flakes = 10 
 snowfall_speed = 1
 
@@ -19,16 +19,20 @@ for i in range (snow_flakes):
     
 def draw(canvas):
     global snow_y
-    #draw background (lignt blue for sky) 
-    canvas.draw_polygon([(0,0), (width, 0), (width, height), (0, height)], 1, "white", "lightblue")
+    # Draw Background (ground) 
+    canvas.draw_polygon([(0,450), (0, 600), (600, 600), (600, 450)], 1, "grey", "grey")
+    # Draw Spruce tree
+    canvas.draw_polygon([(480,400), (480, 450), (500, 450), (500, 400)], 1, "brown", "brown")
+    canvas.draw_polygon([(490,300), (440, 400), (540, 400)], 1, "black", "green")
+    # Draw Snowfall
     for i in range(snow_flakes):
         snowflake_x = random.randint(0, width) 
-        snowflake_y = random.randint(0, height) 
-        canvas.draw_circle((snowflake_x, snow_y[i]), 5, 1, "white", "white")
+        snowflake_y = random.randint(0, height - 150) 
+        canvas.draw_circle((snowflake_x, snowflake_y), 5, 1, "white", "white")
 
         
 frame = simplegui.create_frame("Winter Animation", width, height)
-frame.set_canvas_background("white")
+frame.set_canvas_background("lightblue")
 frame.set_draw_handler(draw)
 frame.start()
 
