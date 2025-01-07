1f694720699cbcbb612b992157b09a1fad6cfc9b
D. Sukhdeo <144715223+DeveshSukhdeo@users.noreply.github.com>
2025-01-07 09:32:31 -0500

    Create CFU18.py

---
+++ Unit8/CFU18.py
@@ -0,0 +1,13 @@
+#Devesh Sukhdeo 
+#Period 1-2
+#1/7/24
+#CFU #18
+'''
+Write a program that adds up all of the prices in the list.
+Hint: Use a for loop
+'''
+
+prices = [1.95, 4.50, 0.99, 5.99]
+
+for i in prices:
+    print(i)
