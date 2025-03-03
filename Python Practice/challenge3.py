210e86d5b96765c1d324d9b35d2148b571bac105
D. Sukhdeo <144715223+DeveshSukhdeo@users.noreply.github.com>
2025-03-03 08:38:18 -0500

    Create challenge3.py

---
+++ Python Practice/challenge3.py
@@ -0,0 +1,21 @@
+def do_twice(f, value):
+    f(value)
+    f(value)
+    
+def print_twice(value):
+    print(value)
+    print(value)
+
+do_twice(print_twice, "spam") 
+
+def do_four(f, value):
+    do_twice(f, value)
+    do_twice(f, value)
+    
+do_four(print_twice, "spam") 
+
+
+
+
+
+    
