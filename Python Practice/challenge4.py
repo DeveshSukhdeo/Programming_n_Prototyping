f584c5e1451fc94db1e5f4ff81bb47cf53f254c0
D. Sukhdeo <144715223+DeveshSukhdeo@users.noreply.github.com>
2025-03-06 08:12:10 -0500

    Create challenge4.py

---
+++ Python Practice/challenge4.py
@@ -0,0 +1,29 @@
+# Devesh Sukhdeo 
+# Period 1-1
+# 3/6/25
+# Python Coding Challenge 3 
+
+def make_grid():
+    print ("+----+----+----+----+")
+    print ("|    |    |    |    |")
+    print ("|    |    |    |    |")
+    print ("|    |    |    |    |")
+    print ("|    |    |    |    |")
+    print ("+----+----+----+----+")
+    print ("|    |    |    |    |")
+    print ("|    |    |    |    |")
+    print ("|    |    |    |    |")
+    print ("|    |    |    |    |")
+    print ("+----+----+----+----+")
+    print ("|    |    |    |    |")
+    print ("|    |    |    |    |")
+    print ("|    |    |    |    |")
+    print ("|    |    |    |    |")
+    print ("+----+----+----+----+")
+    print ("|    |    |    |    |")
+    print ("|    |    |    |    |")
+    print ("|    |    |    |    |")
+    print ("|    |    |    |    |")
+    print ("+----+----+----+----+")
+    
+make_grid() 
