37675ad8a68b8226a1a6b70db7d33304bdc445e5
D. Sukhdeo <144715223+DeveshSukhdeo@users.noreply.github.com>
2025-03-07 08:20:34 -0500

    Update challenge4.py

--- Python Practice/challenge4.py
+++ Python Practice/challenge4.py
@@ -18,3 +18,41 @@ def make_grid():
     print ("+----+----+----+----+")
     
 four(make_grid)
+
+
+
+
+# ------------
+# Answer
+# ------------
+
+def print_beam():
+    print("+----", end=" ") 
+def print_post():
+    print("|    ", end=" ")
+def do_twice(f):
+    f()
+    f()
+def print_beams():
+    do_twice(print_beam)
+    print("+")
+def print_posts():
+    do_twice(print_post)
+    print("|")
+def do_four(f):
+    do_twice(f)
+    do_twice(f)
+def row():
+    print_beams()
+    do_four(print_posts)
+def print_grid():
+    do_twice(row)
+    print_beams()
+    
+print_grid()
+print (" ") 
+def print_grid_twice():
+    do_four(row)
+    print_beams()
+
+print_grid_twice()    
